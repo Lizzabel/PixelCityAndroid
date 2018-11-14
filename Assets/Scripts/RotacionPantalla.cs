d1e00cb9f098c17ffbe8b753c238e6070f2172eb
FactualDeveloper <factualdeveloper@gmail.com>
2018-11-14 15:02:15 -0500

    Cambios en Factual

---
+++ Assets/Scripts/RotacionPantalla.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionPantalla : MonoBehaviour {
+
+	public bool Portrait;
+	public bool LandScapeRight;
+
+	void Awake()
+	{
+		Screen.autorotateToPortrait = Portrait;
+        Screen.autorotateToPortraitUpsideDown = false;
+		Screen.autorotateToLandscapeRight = LandScapeRight;
+        Screen.autorotateToLandscapeLeft = false;
+	}
+}
