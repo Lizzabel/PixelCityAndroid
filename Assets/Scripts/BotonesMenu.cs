f7923dbe8a9ecf71986210422f6eddbd75c93c06
Thokora <fawergilthokora@hotmail.com>
2018-11-27 22:51:24 -0500

    sonidos y otros

--- Assets/Scripts/BotonesMenu.cs
+++ Assets/Scripts/BotonesMenu.cs
@@ -14,9 +14,16 @@ public class BotonesMenu : MonoBehaviour {
     public GameObject panelOpciones;
     AudioSource elAudio;
 
+    public GameObject botonVolumen;
+    Image imgVolumen;
+    int numeroMute = 1;
+    public Sprite[] muted;
+
     private void Start()
     {
         elAudio = gameObject.GetComponent<AudioSource>();
+        imgVolumen = botonVolumen.GetComponent<Image>();
+        imgVolumen.sprite = muted[0];
     }
     public void Jugar()
     {
@@ -47,6 +54,13 @@ public class BotonesMenu : MonoBehaviour {
     public void MutearLosAudios()
     {
         elAudio.mute = !elAudio.mute;
+        imgVolumen.sprite = muted[numeroMute];
+        numeroMute++;
+        if (numeroMute >= muted.Length)
+        {
+            numeroMute = 0;
+        }
     }
+
 
 }
---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuPausa : MonoBehaviour {
+    public GameObject MenuPausaUI;
+    AudioSource elAudio;
+
+    public GameObject botonVolumen;
+    Image imgVolumen;
+    int numeroMute = 1;
+    public Sprite[] muted;
+
+    private void Start()
+    {
+        elAudio = gameObject.GetComponent<AudioSource>();
+        imgVolumen = botonVolumen.GetComponent<Image>();
+        imgVolumen.sprite = muted[0];
+    }
+
+    public void ActivarPausa()
+    {
+        MenuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void DesactivarPausa()
+    {
+        MenuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void ActivarMenuPrincipal()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void MutearLosAudios()
+    {
+        //elAudio.mute = !elAudio.mute;
+        imgVolumen.sprite = muted[numeroMute];
+        numeroMute++;
+        if (numeroMute >= muted.Length)
+        {
+            numeroMute = 0;
+        }
+    }
+}
