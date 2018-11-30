d4cd77022e9b4b0711dc3a28144325924d0000b5
FactualDeveloper <factualdeveloper@gmail.com>
2018-11-30 13:39:23 -0500

    Cambios 30/11/2018 _ 1:40 am

---
+++ Assets/Scripts/EnemigoBajito.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoBajito : MonoBehaviour
+{
+
+    Transform Transplayer;
+	//
+	Animator animEnemigo;
+
+    public bool cerca;
+    Rigidbody2D rbenemigo;
+    public float velocidad;
+
+    float pos;
+	float direccion;
+
+    private void Start()
+    {
+        Transplayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		animEnemigo = gameObject.GetComponent<Animator>();
+        rbenemigo = gameObject.GetComponent<Rigidbody2D>();
+		cerca = true;
+        Girar();
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisionEnemigo)
+    {
+        if (colisionEnemigo.gameObject.tag == "Arepa")
+        {
+			Girar();
+			StartCoroutine(EsperarCerca());
+        }
+        
+        if (colisionEnemigo.gameObject.tag == "Player")
+        {
+            StartCoroutine(EsperarCerca());
+        }
+
+    }
+
+    IEnumerator EsperarCerca()
+    {
+        cerca = false;
+		animEnemigo.speed = 0;
+        yield return new WaitForSecondsRealtime(0.5f);
+        cerca = true;
+		animEnemigo.speed = 1;
+    }
+
+    private void Update()
+    {
+        Correr();
+    }
+
+    public void Correr()
+    {
+        if (!BrayanPropiedades.Muerto)
+        {
+			if (cerca)
+			{
+				
+				if (pos > 0)
+                {
+					direccion = pos++;
+                }
+                else if (pos < 0)
+                {
+					direccion = pos--;
+                }
+				transform.position = Vector2.MoveTowards(transform.position,
+				                             new Vector2(direccion, transform.position.y), velocidad * Time.deltaTime);
+			}
+        }
+    }
+       
+    public void Girar()
+    {
+		pos = Transplayer.position.x - transform.position.x;
+
+        Vector2 giro = gameObject.transform.localScale;
+        //giro.x *= -1;
+
+        if (pos > 0)
+        {
+            giro.x = 3;
+        }
+        else if (pos < 0)
+        {
+            giro.x = -3;
+        }
+        gameObject.transform.localScale = giro;
+    }
+
+}
---
+++ Assets/Scripts/Ganar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ganar : MonoBehaviour {
+
+	public int numeroNivel;
+
+	void Start()
+    {
+        PlayerPrefs.SetInt("Nivel" + (numeroNivel + 1), 1);
+        PlayerPrefs.SetInt("Nivel" + numeroNivel + "_score", PuntosVida.puntos);
+    }
+
+	private void OnTriggerEnter2D(Collider2D Trigger)
+	{
+		if (Trigger.gameObject.tag == "Player")
+		{
+			GanarPuntos();
+		}
+	}
+       
+    public void GanarPuntos()
+    {
+        PlayerPrefs.SetInt("Nivel" + numeroNivel + "_score", PuntosVida.puntos);
+        Initiate.Fade("Menu", Color.black, 1f);
+    }
+}
