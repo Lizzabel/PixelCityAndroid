c730e1532afa36cf1996c7016171d7fcfcf961bb
Isa <isabelhernandezescobar@hotmail.com>
2018-12-13 16:45:16 -0500

    power up

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public static bool BoolPower = false;
+
+    Collider2D colPower;
+    SpriteRenderer rendererPower;
+
+    void Start()
+    {
+        colPower = gameObject.GetComponent<Collider2D>();
+        rendererPower = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rendererPower.enabled = false;
+            BoolPower = true;
+            StartCoroutine(BoolFalse());
+            colPower.enabled = false;
+        }
+    }
+    IEnumerator BoolFalse()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        BoolPower = false;
+        Destroy(gameObject);
+    }
+}
